25a87353590c8d3464ecb47563e358cff8a1849b
felipe <felipevdcarneiro@gmail.com>
2019-06-22 12:00:44 -0300

    half of authentication part 1

---
+++ section 4 - flask-restful/code/security.py
@@ -0,0 +1,21 @@
+users = [
+    {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+]
+
+username_mapping = {'bob': {
+    'id': 1,
+    'username': 'bob',
+    'password': 'asdf'
+}}
+
+
+
+userid_mapping = {1: {
+    'id': 1,
+    'username': 'bob',
+    'password': 'asdf'
+}}
