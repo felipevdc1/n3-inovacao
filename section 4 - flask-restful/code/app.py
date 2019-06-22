acf8481c3d7fff04737214cd4be2f9ad3c6bf9e8
felipe <felipevdcarneiro@gmail.com>
2019-06-22 11:40:17 -0300

    code improve and error control

--- section 4 - flask-restful/code/app.py
+++ section 4 - flask-restful/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -9,17 +9,25 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404    
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
 
-api.add_resource(Item, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
