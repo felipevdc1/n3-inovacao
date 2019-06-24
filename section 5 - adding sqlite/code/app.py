b72d730f0a02cb39c9846a2ec782421f6a5b04e0
felipe <felipevdcarneiro@gmail.com>
2019-06-24 00:57:16 -0300

    delete fix

--- section 5 - adding sqlite/code/app.py
+++ section 5 - adding sqlite/code/app.py
@@ -18,4 +18,6 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+# if usado para evitar que caso seja importado, o server seja reiniciado
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
